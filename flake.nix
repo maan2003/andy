6b2446c559d0055581bd061e8b0d8ae459b94dfe
maan2003 <manmeetmann2003@gmail.com>
2026-03-17 19:06:49 +0000

    chore: 0.4

--- flake.nix
+++ flake.nix
@@ -5,14 +5,14 @@
 
   outputs = { nixpkgs, ... }:
     let
-      base = "https://github.com/maan2003/andy/releases/download/v0.3.0";
+      base = "https://github.com/maan2003/andy/releases/download/v0.4.0";
       pkg = system: name: sha256:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
         pkgs.stdenv.mkDerivation {
           pname = "andy";
-          version = "0.3.0";
+          version = "0.4.0";
           src = pkgs.fetchurl {
             url = "${base}/${name}.tar.xz";
             inherit sha256;
@@ -27,10 +27,10 @@
     in
     {
       packages = {
-        x86_64-linux.default = pkg "x86_64-linux" "andy-x86_64-unknown-linux-musl" "0e8b6aafc58f147c012e66f194464660d29c95e567f94338c0a993b5b7c73357";
-        aarch64-linux.default = pkg "aarch64-linux" "andy-aarch64-unknown-linux-musl" "cff3c2d42f365ad4f7eeb1fe8ca816742bec7c85b093f704c46d9fa3b7ee6cb5";
-        x86_64-darwin.default = pkg "x86_64-darwin" "andy-x86_64-apple-darwin" "f57971f14dd1a17bb0b1fbfb0a4f3c67d9883834410ac68e2fa2a09196969c19";
-        aarch64-darwin.default = pkg "aarch64-darwin" "andy-aarch64-apple-darwin" "e3655eb066dfa2415cdccbf3a31e7294fbdc64ddb55b3e94f26ba2f6f877d985";
+        x86_64-linux.default = pkg "x86_64-linux" "andy-x86_64-unknown-linux-musl" "0b8e83ec45eeee7f916165c52b1cb93aca5192cb1d1a4f54ce72f70fe9d324c2";
+        aarch64-linux.default = pkg "aarch64-linux" "andy-aarch64-unknown-linux-musl" "6a3c4f4bcc73efefe542741b28117fb868520dead52e4d31f948b28f1197480b";
+        x86_64-darwin.default = pkg "x86_64-darwin" "andy-x86_64-apple-darwin" "2cc28c14bb2f538298f1913d36d4f2f122e90d782cef2fd163a4201381d852b5";
+        aarch64-darwin.default = pkg "aarch64-darwin" "andy-aarch64-apple-darwin" "f87c6435c77533e24838c23387bc7b37d5b1c627069c42033e5ec42457a1cd34";
       };
     };
 }
